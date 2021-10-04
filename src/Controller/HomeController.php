ed6284d1563bae179499e1be8ddf24da75a2ee62
Gerrit-Authentic <verbis.re@gmail.com>
2021-10-04 12:30:53 +0200

    modifications mineures base.html.twig

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,9 +1,14 @@
 <?php
 
 namespace App\Controller;
-
+use app\Entity\Fans;
+use App\Form\ContactFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Persistence\ObjectManager as PersistenceObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
@@ -12,6 +17,47 @@ class HomeController extends AbstractController
     public function index(): Response
     {
         return $this->render('home/homepage.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+      
+    #[Route('/legal', name: 'legal')]
+    public function legal(): Response
+    {
+        return $this->render('home/mentions-legales.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+    #[Route('/services', name: 'services')]
+    public function services(): Response
+    {
+        return $this->render('home/nos-services.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+    #[Route('/portefolio', name: 'portefolio')]
+    public function portefolio(): Response
+    {
+        return $this->render('home/portefolio.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+    #[Route('/contact', name: 'contact')]
+    public function forms() :response
+ {   
+     
+             $form = $this ->createForm(ContactFormType::class );
+             
+          return $this->render('home/contact.html.twig', [
+            'controller_name' => 'HomeController',
+            'form'=>$form->createView()
+        ]);
+    }
+    
+    #[Route('/aboutus', name: 'aboutus')]
+    public function aboutus(): Response
+    {
+        return $this->render('home/a-propos.html.twig', [
             'controller_name' => 'HomeController',
         ]);
     }
---
+++ src/Entity/Fans.php
@@ -0,0 +1,194 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FansRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=FansRepository::class)
+ */
+class Fans
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=10)
+     */
+    private $civilites;
+
+    /**
+     * @ORM\Column(type="string", length=20)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="string", length=20)
+     */
+    private $prenom;
+
+    /**
+     * @ORM\Column(type="string", length=20)
+     */
+    private $telephone;
+
+    /**
+     * @ORM\Column(type="string", length=20)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $message;
+
+    /**
+     * @ORM\Column(type="string", length=20)
+     */
+    private $lieu;
+
+    /**
+     * @ORM\Column(type="string", length=20)
+     */
+    private $type;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $date;
+
+    /**
+     * @ORM\Column(type="string", length=20, nullable=true)
+     */
+    private $fonction;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCivilites(): ?string
+    {
+        return $this->civilites;
+    }
+
+    public function setCivilites(string $civilites): self
+    {
+        $this->civilites = $civilites;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): self
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getTelephone(): ?string
+    {
+        return $this->telephone;
+    }
+
+    public function setTelephone(string $telephone): self
+    {
+        $this->telephone = $telephone;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(string $message): self
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    public function getLieu(): ?string
+    {
+        return $this->lieu;
+    }
+
+    public function setLieu(string $lieu): self
+    {
+        $this->lieu = $lieu;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getFonction(): ?string
+    {
+        return $this->fonction;
+    }
+
+    public function setFonction(?string $fonction): self
+    {
+        $this->fonction = $fonction;
+
+        return $this;
+    }
+}
