25a80f2e8efa366229f933e45e21722fde6c7ff9
Gerrit-Authentic <verbis.re@gmail.com>
2021-10-07 22:31:13 +0200

    Margin et padding dans le base. Suggestions de contenu. Ajustements avec Boottrap.

--- src/Form/FparticulierType.php
+++ src/Form/FparticulierType.php
@@ -5,7 +5,10 @@ namespace App\Form;
 use App\Entity\Particulier;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class FparticulierType extends AbstractType
