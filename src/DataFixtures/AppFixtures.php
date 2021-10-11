8fb00b9735758fef686736bf4cb5abd7fd923eea
DannyPoizot <poizot.dan@gmail.com>
2021-10-11 09:55:38 +0200

    fixtures en cours

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,16 +4,11 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
-  private $encoder;
-
-  public function __construct(UserPasswordHasherInterface $encoder)
-  {
-      $this->encoder = $encoder;
-  }
-
+  
     public function load(ObjectManager $manager)
     {
         // $product = new Product();
