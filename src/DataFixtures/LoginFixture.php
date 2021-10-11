f087724d8af3b6dc1084d2c0f26adb12a447f96e
DannyPoizot <poizot.dan@gmail.com>
2021-10-11 09:55:38 +0200

    fixtures en cours

--- src/DataFixtures/LoginFixture.php
+++ src/DataFixtures/LoginFixture.php
@@ -5,17 +5,24 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use app\Entity\Login;
-use app\src\DataFixtures\AppFixtures;
+use App\DataFixtures\AppFixtures;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
 
 class LoginFixture extends Fixture
 {
+    private $encoder;
 
+    public function __construct(UserPasswordHasherInterface $encoder)
+    {
+      $this->encoder = $encoder;
+    }
 
     public function load(ObjectManager $manager): void
     {
         $login = new Login();
           $login->setLogin("admin@test.com");
-          $login->setPassword($this->encoder->encodePassword($login, '123456'));
+          $login->setPassword($this->encoder->encodePassword($login));
 
 
         // $product = new Product();
