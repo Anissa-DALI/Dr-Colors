8b93c8c0a097878bcf37c70c3fa73dee99867ad7
Kevin Loth <kevin.loth@hotmail.fr>
2021-10-13 10:11:29 +0200

    reduction taille images

--- config/bundles.php
+++ config/bundles.php
@@ -13,6 +13,5 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
-    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
 ];
