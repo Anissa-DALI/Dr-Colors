990ea9b7c66aa9228fb761a117bdd933b8fc8978
Matthieu Mota <matthieumota@gmail.com>
2021-10-13 16:30:59 +0200

    Fix migrations

--- migrations/Version20211009150746.php
+++ migrations/Version20211009150746.php
@@ -22,9 +22,6 @@ final class Version20211009150746 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE personne (id INT AUTO_INCREMENT NOT NULL, civilites VARCHAR(20) NOT NULL, nom VARCHAR(20) NOT NULL, prenom VARCHAR(20) NOT NULL, telephone VARCHAR(20) NOT NULL, email VARCHAR(20) NOT NULL, realisation VARCHAR(255) DEFAULT NULL, message LONGTEXT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE prive (id INT AUTO_INCREMENT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE admin CHANGE roles roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\'');
-        $this->addSql('DROP INDEX uniq_880e0d76ea53e129 ON admin');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_880E0D76E7927C74 ON admin (email)');
     }
 
     public function down(Schema $schema): void
@@ -32,8 +29,5 @@ final class Version20211009150746 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('DROP TABLE personne');
         $this->addSql('DROP TABLE prive');
-        $this->addSql('ALTER TABLE admin CHANGE roles roles LONGTEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_bin`');
-        $this->addSql('DROP INDEX uniq_880e0d76e7927c74 ON admin');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_880E0D76EA53E129 ON admin (email)');
     }
 }
