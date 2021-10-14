37ab4abbe1f546595d2544f5ba346546503b9a59
Anissa Dali <anissa.artshop@gmail.com>
2021-10-14 15:20:18 +0200

    FormPro

--- src/Controller/FormProController.php
+++ src/Controller/FormProController.php
@@ -7,6 +7,7 @@ use App\Entity\Comment;
 use App\Controller\SluggerInterface;
 use App\Controller\FileException;
 use App\Controller\Download;
+use App\Entity\Image;
 use App\Form\FormProType;
 use App\Form\CommentFormType;
 use ContainerAYRoeVT\getVichUploader_Form_Type_ImageService;
@@ -14,8 +15,10 @@ use ContainerE4ZmX7x\getForm_Type_FormService;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormView;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Routing\Annotation\Route;
@@ -35,49 +38,21 @@ class FormProController extends AbstractController
             $form = $this -> createForm(FormProType::class, $professionnels);
             
             $form->handleRequest($request);
+
             if ($form->isSubmitted() && $form->isValid()){
+                $imageFile = $form -> get('imageFile') -> getData();
             $entityManager->persist($professionnels);
             $entityManager->flush();
-    }
 
-            return $this->render('form_pro/index.html.twig', [
+            $this->addFlash('success', 'Votre demande a été envoyé.');
+
+            }
+
+            return $this->render('form_pro/index.html.twig', [ 
                 'form' => $form->createView(),
-            ]);
-        ;   
-
-    }}
-
-    // #[Route('/form/pro/telecharger', name: 'telecharger_photo')]
-  
-    // public function new(Request $request, SluggerSluggerInterface $slugger)
-    // {
-    //     $download = new Downloader ('null', null);
-    //     $form = $this->createForm(ProductType::class, $download);
-    //     $form->handleRequest($request);
-
-    //     if ($form->isSubmitted() && $form->isValid()) {
-
-    //         /** @var UploadedFile $telecharger_photo */
-
-    //         $download = $form->get('telecharger_photo')->getData();
-
-    //         if ($telecharger_photo) {
-    //             $originalFilename = pathinfo($telecharger_photo->getClientOriginalName(), PATHINFO_FILENAME);
-                
-    //             $safeFilename = $slugger->slug($originalFilename);
-
-                
-                // try {
-                //     $telecharger_photo->move(
-                //         $this->getParameter('download_directory'),
-                //     );
-                // }
-                // catch {
-
-                // };
-
-        // return $this->render('form_pro/index.html.twig', [
-        //     'form' => $form->createView(),
-
-        // ]);
+                ]);  
+            }
+    }
+        
+    
     
--- src/Form/ImageType.php
+++ src/Form/ImageType.php
@@ -7,6 +7,7 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Vich\UploaderBundle\Form\Type\VichImageType;
 
 
@@ -15,22 +16,26 @@ class ImageType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-        ->add('telecharger_photo', VichImageType::class, [
+        ->add('imageFile', VichImageType::class, [
             'label' => 'Telecharger photo',
 
-            'mapped' => false,
             'required' => false,
             'constraints' => [
                 new File([
                     'maxSize' => '1024k',
                     'mimeTypes' => [
-                        'application/pdf',
-                        'application/x-pdf',
+                        'image/jpeg',
+                        'image/jpg',
+                        'image/png',
                         ],
-                    'mimeTypesMessage' => 'Please upload a valid PDF document',
+                    'mimeTypesMessage' => 'Ce fichier doit être une image',
             ])
         ],
     ]);
 
     }
+
+    
+
+    
 }
